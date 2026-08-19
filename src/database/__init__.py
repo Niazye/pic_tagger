a32ec3b1861d87a878ee4ed823c7a306c9c9060d
niazye <niazye@outlook.com>
2026-08-19 17:55:04 +0800

    bug: 调整了两个initpy的导入顺序以消除循环导入

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,7 +1,7 @@
 """数据库层：连接管理、建表、数据访问。"""
 
-from src.database.connection import Database, get_db, get_connection
 from src.database.schema import create_tables, DEFAULT_CATEGORY_ID, DEFAULT_CATEGORY_NAME
+from src.database.connection import Database, get_db, get_connection
 from src.database.repository import (
     CategoryRepository,
     TagRepository,
